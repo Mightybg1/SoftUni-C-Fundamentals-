7fe214598b4d91e435807b36332cc25191227306
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-13 08:00:49 +0300

    Add files via upload

---
+++ Second lecture / Exercise/10 - RageExpenses/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _10___RageExpenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headphonesPrice = double.Parse(Console.ReadLine());
+            double moucePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadset = 0;
+            int trashedMouce = 0;
+            int trashedKeyboard = 0;
+            int trashedDisplay = 0;
+
+            for(int i = 1; i <= lostGames; i++)
+            {
+                if((double)i % 2 == 0)
+                {
+                    trashedHeadset++;
+                }
+
+                if ((double)i % 3 == 0)
+                {
+                    trashedMouce++;
+                }
+
+                if((double)i % 2 == 0 && (double)i % 3 == 0)
+                {
+                    trashedKeyboard++;
+                }
+
+                if((double)i % 12 == 0)
+                {
+                    trashedDisplay++;
+                }
+            }
+            
+            double price = (headphonesPrice * trashedHeadset) + (moucePrice * trashedMouce) + (keyboardPrice * trashedKeyboard) + (displayPrice * trashedDisplay);
+            Console.WriteLine($"Rage expenses: {price:f2} lv.");
+        }
+    }
+}
+
+            //for (double i = 0; i <= lostGames; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        trashedHeadset++;
+            //    }
+            //    for (int j = 0; j <= lostGames; j++)
+            //    {
+            //        if (i % 3 == 0)
+            //        {
+            //            trashedMouce++;
+            //        }
+
+            //        if (i % 2 == 0 && i % 3 == 0)
+            //        {
+            //            trashedKeyboard++;
+            //        }
+
+            //        if(trashedKeyboard % 2 == 0)
+            //        {
+            //            trashedDisplay++;
+            //        }
+            //    }
+            //}
---
+++ Second lecture / Exercise/11 - Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _11___Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double pricePerCapsule = 0;
+            int days = 0;
+            int capsulesCount = 0;
+            double sum = 0;
+            double total = 0;
+
+            for (int i = 1; i <= orders; i++)
+            {
+                if (i > 0)
+                {
+                    pricePerCapsule = double.Parse(Console.ReadLine());
+                    days = int.Parse(Console.ReadLine());
+                    capsulesCount = int.Parse(Console.ReadLine());
+
+                    sum = ((days * capsulesCount) * pricePerCapsule);
+
+                    Console.WriteLine($"The price for the coffee is: {sum:f2}");
+                }
+
+                if (orders >= 1)
+                {
+                    total += sum;
+                }
+            }
+            Console.WriteLine($"Total: {total:f2}");
+        }
+    }
+}
