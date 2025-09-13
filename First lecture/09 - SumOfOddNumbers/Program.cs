f331bb12a513f1c36b669e8395ad9386743d5639
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-13 07:50:54 +0300

    Add files via upload

---
+++ First lecture/09 - SumOfOddNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _09___SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input = int.Parse(Console.ReadLine());
+            int Numbers = 1;
+            int Sum = 0;
+
+            for (int i = 1; i <= Input;)
+            {
+                if (Numbers % 2 == 1)
+                {
+                    Console.WriteLine(Numbers);
+                    Sum += Numbers;
+                    i++;
+                }
+                Numbers++;
+            }
+            Console.WriteLine($"Sum: {Sum}");
+
+        }
+    }
+}
---
+++ First lecture/10 - MultiplicationTable/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _10___MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Input = int.Parse(Console.ReadLine());
+
+            for( int i = 1; i<= 10; i++)
+            {
+                Console.WriteLine($"{Input} X {i} = {Input*i}");
+            }
+        }
+    }
+}
