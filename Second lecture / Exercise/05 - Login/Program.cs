add129ca20b8ce47fcd0883f65b866539d955273
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-13 07:59:10 +0300

    Add files via upload

---
+++ Second lecture / Exercise/05 - Login/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05___Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            string password = new string(user.Reverse().ToArray());
+
+            for (int i = 1; i <= 4; i++)
+            {
+                string passwordAttempt = Console.ReadLine();
+                if (passwordAttempt != password && i <= 3)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else if (passwordAttempt == password)
+                {
+                    Console.WriteLine($"User {user} logged in.");
+                    break;
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine($"User {user} blocked!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Second lecture / Exercise/06 - StrongNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Channels;
+
+namespace StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int number = input;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int digit = number % 10; 
+                number /= 10;            
+
+                
+                int factorial = 1;
+                for (int i = 1; i <= digit; i++)
+                {
+                    factorial *= i;
+                }
+
+                sum += factorial; 
+            }
+
+            if (sum == input)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
