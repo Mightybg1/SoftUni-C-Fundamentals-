ab4269590f0e9c54f26c8b6a77db673cebd950cf
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-13 07:57:05 +0300

    Add files via upload

---
+++ Second lecture / Exercise/02 - division/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02___division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 10 == 0)
+            {
+                Console.WriteLine("The number is devisible by 10");
+            }
+            else if (input % 7 == 0)
+            {
+                Console.WriteLine("The number is devisible by 7");
+            }
+            else if (input % 6 == 0)
+            {
+                Console.WriteLine("The number is devisible by 6");
+            }
+            else if (input % 3 == 0)
+            {
+                Console.WriteLine("The number is devisible by 3");
+            }
+            else if (input % 2 == 0)
+            {
+                Console.WriteLine("The number is devisible by 2");
+            }
+            else
+            {
+                Console.WriteLine("Not devisible");
+            }
+
+
+            
+        }
+    }
+}
