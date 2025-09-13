ddd48c648d05277388d357fcec51fc26c8b3069f
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-13 07:47:56 +0300

    Add files via upload

---
+++ First lecture/01 - StudentInformation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Grade = double.Parse(Console.ReadLine());
+
+            if(Grade< 3.00)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
---
+++ First lecture/03 - PassedOrFailed/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03___PassedOrFailed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Grade = double.Parse(Console.ReadLine());
+
+            if (Grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
