0cc1d46e19952b9e850f582ded6f5e36e755a9a1
Mightybg1 <118848461+Mightybg1@users.noreply.github.com>
2025-09-14 03:43:48 +0300

    Add files via upload

---
+++ Second lecture /Bonus exercise/04 - ReverseString/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _04___ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = new string(input.Reverse().ToArray());
+            Console.WriteLine(output);
+        }
+    }
+}
